a2bc558a93dcf1fa76712fdb1efec6cfad8bd9fe
Pablo Duran <josepablo50@hotmail.com>
2023-07-17 22:10:21 -0600

    Cambios en factura.go
    Cambios en factura.go

--- Basico/11 Structs - Tipos personalizados/factura.go
+++ Basico/11 Structs - Tipos personalizados/factura.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Un struct (abreviado de la palabra structure) sirve para crear un miembro con diferentes tipos de datos dentro de una misma variable, a diferencia de los Array que solo almacenan datos del mismo tipo. No es una clase (GO no usa clases)
 
 type factura struct {
@@ -16,4 +21,52 @@ func nuevaFactura(nombre string) factura {
 	}
 
 	return fact
+}
+
+// funcion para agregar datos a la factura
+// utilizamos un "Receiver" para que esta funcion este siempre asociada con los struct facturas y no sea llamado por cualquier otro tipo
+func (f *factura) formatoFactura() string {
+	// *** Esta funcion tambien funciona sin el puntero en el "Receiver", pero podria afectar el rendimiento de la aplicacion si se usa constantemente, ya que crearia una copia cada vez que se llama, lledando la memoria de muchas de ellas
+
+	ff := "Detalle de la factura: \n"
+	var total float64 = 0
+
+	// agregar los items a la variable ff
+	for k, v := range f.items {
+		ff += fmt.Sprintf("%-25v ...$%v \n", k+":", v) // con el '-25' despues del signo de '%' le decimos que tome un espacio de 25 caracteres, o sea, la palabra que es el key (cafe, pie, etc) mas lo que haga falta para llegar a 25 en espacios en blanco, es una especie de tabulado, asi los precios se van a ver alineados
+		total += v
+	}
+
+	// agregar el Tip a la factura
+	ff += fmt.Sprintf("%-25v ...$%0.2f \n", "Tip:", f.tip)
+
+	// agregar el total a la factura
+	ff += fmt.Sprintf("%-25v ...$%0.2f \n", "Total sin Tip:", total)
+
+	// total + tip
+	ff += fmt.Sprintf("%-25v ...$%0.2f \n", "Total con Tip:", total+f.tip)
+
+	return ff
+}
+
+// como se vio en el modulo de "Pass by value", los struct son elementos que no se actualizan cuando se pasan por parametro, lo que se pasa es una copia de ese elemento, es por eso que debemos pasar un puntero con la direccion del elemento para que estas funciones de abajo actualizen el elemento como tal, esto se logra agregando un * al tipo en el "Receiver"
+
+func (f *factura) actualizarTip(tip float64) {
+	f.tip = tip
+}
+
+func (f *factura) agregarItem(nombre string, precio float64) {
+	f.items[nombre] = precio
+}
+
+func (f *factura) salvarFactura() {
+	datos := []byte(f.formatoFactura())
+
+	error := os.WriteFile("facturas/"+f.nombre+".txt", datos, 0644)
+
+	if error != nil {
+		panic(error)
+	}
+
+	fmt.Println("La factura se salvo.")
 }
--- Basico/11 Structs - Tipos personalizados/main.go
+++ Basico/11 Structs - Tipos personalizados/main.go
@@ -1,9 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func obtenerInput(promt string, r *bufio.Reader) (string, error) {
+	fmt.Print(promt)
+
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func crearFactura() factura {
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Digite el nombre de la factura: ")
+	// nombre, _ := reader.ReadString('\n')
+	// nombre = strings.TrimSpace(nombre)
+
+	nombre, _ := obtenerInput("Digite el nombre de la factura: ", reader)
+
+	f := nuevaFactura(nombre)
+
+	fmt.Println("Se creo la factura para ", f.nombre)
+
+	return f
+}
+
+func promtOptions(f factura) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opc, _ := obtenerInput("Elija una opcion (a - Agregar item, g - Guardar factura, t - Agregar tip): ", reader)
+
+	switch opc {
+	case "a":
+		nombre, _ := obtenerInput("Nombre del item: ", reader)
+		precio, _ := obtenerInput("Precio del item: ", reader)
+
+		precioParseado, err := strconv.ParseFloat(precio, 64)
+
+		if err != nil {
+			fmt.Println("El precio debe ser un numero")
+			promtOptions(f)
+		}
+
+		f.agregarItem(nombre, precioParseado)
+
+		fmt.Println("Item agregado - ", nombre, precio)
+		promtOptions(f)
+	case "t":
+		tip, _ := obtenerInput("Digite el monto del tip: ", reader)
+
+		tipParseado, err := strconv.ParseFloat(tip, 64)
+
+		if err != nil {
+			fmt.Println("El tip debe ser un numero")
+			promtOptions(f)
+		}
+
+		f.actualizarTip(tipParseado)
+
+		fmt.Println("Tip agregado - ", tip)
+		promtOptions(f)
+	case "g":
+		f.salvarFactura()
+
+		fmt.Println("Salvaste la factura - ", f.nombre)
+	default:
+		fmt.Println("Opcion invalida")
+		promtOptions(f)
+	}
+}
 
 func main() {
-	miFactura := nuevaFactura("Sergio")
+	miFactura := crearFactura()
+	promtOptions(miFactura)
 
-	fmt.Println(miFactura)
+	// fmt.Println(miFactura)
+
+	// miFactura.agregarItem("Sopa", 4.50)
+	// miFactura.agregarItem("Pie", 8.95)
+	// miFactura.agregarItem("Ensalada", 4.95)
+	// miFactura.agregarItem("Cafe", 3.25)
+
+	// miFactura.actualizarTip(10)
+
+	// fmt.Println(miFactura.formatoFactura())
+
 }
